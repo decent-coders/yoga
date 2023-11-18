d6517289d73d3d129d43510f5db960f1d44d987a
Hridoy Hawladar <148939114+decent-coders@users.noreply.github.com>
2023-11-18 10:38:39 +0600

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,60 @@
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+  
+  // slick slider1
+$('.abc').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false,
+  
+  responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+  ]
+});
+  
+          
+           
+
+  // slick slider2
+$('.cde').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false,
+  
+  responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+  ]
+});
+  
+          
+           
+
